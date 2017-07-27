a84c8500d427ca21216e0035b1b75dd6746e3d3b
zxz <zxzdwilling@126.com>
2017-07-27 19:59:32 +0800

    201707271959

--- app/src/androidTest/java/com/baibai/zxz/zxztest/LoginInstrumentedTest.java
+++ app/src/androidTest/java/com/baibai/zxz/zxztest/LoginInstrumentedTest.java
@@ -25,8 +25,18 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class LoginInstrumentedTest {
 
-    String PACKAGE_NAME = "com.baibai.baibai";
+    //登录的手机号、密码
+    //[0]:手机号, [1]:密码, [2]:对应的测试用例标题
+    String ACCOUNT_PASSWORD[] = {
+            "", "", "手机号和密码为空，登录失败",
+            "", "123456", "手机号为空，登录失败",
+            "18819425652", "123456", "手机号错误，登录失败",
+            "05521397022", "", "手机号正确，密码为空，登录失败",
+            "05521397022", "1234567", "手机号正确，密码为空，登录失败",
+            "05521397022", "123456", "手机号正确，密码正确，登录成功"
+    };
 
+    String PACKAGE_NAME = "com.baibai.baibai";//包名
     Instrumentation mInstrumentation = null;
     UiDevice mDevice = null;
     Context mContext = null;
@@ -66,9 +76,14 @@ public class LoginInstrumentedTest {
             UiObject mSplashPic = mDevice.findObject(new UiSelector()
                     .className("android.widget.FrameLayout").packageName(PACKAGE_NAME));
             if(0 == mSplashPic.getChildCount()){
-
+                long start = SystemClock.uptimeMillis();
+                mSplashPic.waitUntilGone(10 * 1000);
+                long time = SystemClock.uptimeMillis() - start;
+                //判断停留在启动页时间5-10秒之间
+                assertTrue("the Splash time is not bettween 5s and 10s"
+                        , time >= 5 * 1000 && time <= 10 * 1000);
             }
-            long start = SystemClock.uptimeMillis();
+
 
         }
     }
